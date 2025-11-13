1b733a03dd4c351cc81630576bc1535393c6e200
berthashipper <168128913+berthashipper@users.noreply.github.com>
2025-11-13 16:57:46 +0100

    distribution EDA

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('Mental_Health_and_Social_Media_Balance_Dataset.csv')
 print(df.head())
+
+df.hist(figsize=(10,8), bins=20)
+plt.suptitle("Distributions of Numeric Features", fontsize=14)
+plt.show()
